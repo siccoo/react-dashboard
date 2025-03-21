1477b8bcc74ce9e34722c0f12b281cfd956339e1
Chilaka Michael Obinna <obichilaka@gmail.com>
2025-03-21 18:46:34 +0100

    install required libraries

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App: React.FC = () => {
   return (
-    <div>React Dashboard</div>
+    <div className='text-3xl font-bold text-green-400'>React Dashboard</div>
   )
 }
 
